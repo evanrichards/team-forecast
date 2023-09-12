8ee07e92ffb8d38a6cb393e8c4b1d270541b6051
Evan Richards <tevanrichards@gmail.com>
2023-09-11 21:26:45 -0700

    trpc setup

---
+++ app/_trpc/serverClient.ts
@@ -0,0 +1,11 @@
+import { appRouter } from "@/server";
+import { httpBatchLink } from "@trpc/client";
+
+const url =
+  process.env.NODE_ENV === "production"
+    ? "your-production-url/api/trpc"
+    : "http://localhost:3000/api/trpc";
+
+export const serverClient = appRouter.createCaller({
+  links: [httpBatchLink({ url })],
+});
